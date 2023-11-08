8be5b516148a8e9e4eb9dac5acbacf0ce74ac15e
Mara <125482894+Mara2403@users.noreply.github.com>
2023-11-08 12:08:32 +0100

    add caesar_cipher method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,35 @@
 def caesar_cipher(string, key)
+  #user string to ascii array
+  array_ascii = string.bytes
 
+  # encrypt (shift) into numeric ascii array using the key
+  numeric_encrypted_array = array_ascii.map do |item| 
+    shift = item + key
+    if (97..122).include?(item)
+      if (shift > 122)
+        shift - 26
+      else shift
+      end
+    elsif (65..90).include?(item)
+      if (shift > 90)
+        shift - 26
+      else shift
+      end
+    else item
+    end
+  end
+
+  # converting ascii ascii back to a string
+  encrypted_array = numeric_encrypted_array.map do |number| 
+    number.chr
+    end
+    puts encrypted_string = encrypted_array.join
+    return encrypted_string
 end
 
+puts "What is the text you would like to encrypt using Caesar Cipher encryption? Please enter the text below:"  
+user_string = gets.chomp
+puts "Please enter the number you'd like to use for encrypting your text:"
+user_key = gets.chomp.to_i
+
+caesar_cipher(user_string, user_key)
